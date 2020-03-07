bd030cba5605e4ba175429c2a7b7118a86bfde81
mycom <mycom@mycom-PC>
2020-03-08 06:06:17 +0900

    2020/03/08/일요일

---
+++ src/ch3_2/OperatorEx4.java
@@ -0,0 +1,14 @@
+package ch3_2;
+
+public class OperatorEx4 {
+	public static void main(String[] args) {
+		int ii = -10;
+		ii = +ii;
+		System.out.println(ii);
+		
+		ii = -10;
+		ii = -ii;
+		System.out.println(ii);
+	}
+
+}
---
+++ src/ch6_8/PrimitiveParamEx.java
@@ -0,0 +1,24 @@
+package ch6_8;
+
+class Data{
+	int x;
+}
+
+public class PrimitiveParamEx {
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+		
+		change(d.x);
+		System.out.println("After chage(d.x)");
+		System.out.println("main() : x = " + d.x);
+		
+	}
+	
+	static void change(int x) {	// 기본형 매개변수
+		x = 1000;
+		System.out.println("change() : x = " + x);
+	}
+
+}
