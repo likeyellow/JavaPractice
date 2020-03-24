734fa22069385534292b993377a687d36dc5a9c9
mycom <mycom@mycom-PC>
2020-03-24 09:59:52 +0900

    2020/03/19/목요일

---
+++ src/ch6_12/ConstructorTest.java
@@ -0,0 +1,23 @@
+package ch6_12;
+
+class Data1{
+	int value;
+}
+class Data2{
+	int value;
+	
+	Data2(int x){ // 매개변수가 있는 생성자
+		value = x;
+	}
+}
+
+
+public class ConstructorTest {
+
+	public static void main(String[] args) {
+		Data1 d1 = new Data1();
+	//	Data2 d2 = new Data2(); // 컴파일 에러 발생
+	//	Data2 d2 = new Data2(10); // OK
+	}
+
+}
